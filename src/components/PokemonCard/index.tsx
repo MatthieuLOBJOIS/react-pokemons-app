9bacf54527b493f68545120cc2d53ee5b113fb03
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-02-07 10:48:02 +0100

    Refacto page pokemon list

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,12 +1,47 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../../models/pokemon';
+import './style.css';
 
 type Props = {
 	pokemon: Pokemon;
+	borderColor?: string;
 };
 
-const PokemonCard: FunctionComponent<Props> = ({ pokemon }) => {
-	return <div>Ce composant est chargé d'afficher le pokémon : {pokemon.name}</div>;
+const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688' }) => {
+	const [ color, setColor ] = useState<string>();
+
+	const showBorder = () => {
+		setColor(borderColor);
+	};
+
+	const hideBorder = () => {
+		setColor('#f5f5f5'); // On remet la bordure en gris.
+	};
+
+	const formatDate = (date: Date): string => {
+		return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+	};
+
+	return (
+		<div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+			<div className="card horizontal" style={{ borderColor: color }}>
+				<div className="card-image">
+					<img src={pokemon.picture} alt={pokemon.name} />
+				</div>
+				<div className="card-stacked">
+					<div className="card-content">
+						<p>{pokemon.name}</p>
+						<p>
+							<small>{pokemon.types.join(' ')}</small>
+						</p>
+						<p>
+							<small>{formatDate(pokemon.created)}</small>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PokemonCard;
