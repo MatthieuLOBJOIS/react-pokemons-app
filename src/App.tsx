ac43993aeef0acb83c8063f714d58a1d51326746
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-02-07 17:13:18 +0100

    Add route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React, { FunctionComponent } from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PokemonList from './pages/PokemonList';
+import PokemonsDetail from './pages/PokemonsDetail';
 
 const App: FunctionComponent = () => {
-	return <PokemonList />;
+	return (
+		<Router>
+			<div>
+				{/* La barre de navigation commun à toutes les pages */}
+				<nav>
+					<div className="nav-wrapper teal">
+						<Link to="/" className="brand-logo center">
+							Pokédex
+						</Link>
+					</div>
+				</nav>
+				{/* Le système de gestion des routes de notre application */}
+				<Switch>
+					<Route exact path="/" component={PokemonList} />
+					<Route exact path="/pokemons" component={PokemonList} />
+					<Route path="/pokemons/:id" component={PokemonsDetail} />
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -3,6 +3,7 @@ import Pokemon from '../../models/pokemon';
 import './style.css';
 import formatDate from '../../helpers/format-date';
 import formatType from '../../helpers/format-type';
+import { useHistory } from 'react-router-dom';
 
 type Props = {
 	pokemon: Pokemon;
@@ -11,6 +12,7 @@ type Props = {
 
 const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688' }) => {
 	const [ color, setColor ] = useState<string>();
+	const history = useHistory();
 
 	const showBorder = () => {
 		setColor(borderColor);
@@ -20,8 +22,20 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
 		setColor('#f5f5f5'); // On remet la bordure en gris.
 	};
 
+	// Closure
+	const goToPokemon = (id: number) => {
+		return () => {
+			history.push(`/pokemons/${id}`);
+		};
+	};
+
 	return (
-		<div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+		<div
+			className="col s6 m4"
+			onClick={goToPokemon(pokemon.id)}
+			onMouseEnter={showBorder}
+			onMouseLeave={hideBorder}
+		>
 			<div className="card horizontal" style={{ borderColor: color }}>
 				<div className="card-image">
 					<img src={pokemon.picture} alt={pokemon.name} />
