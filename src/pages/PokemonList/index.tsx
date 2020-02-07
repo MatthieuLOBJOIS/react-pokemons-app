36025d72c324c16cc7768a9abe82f58f7e160287
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-02-07 11:36:27 +0100

    creat hooks custom get pokemons

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -2,13 +2,10 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from '../../models/pokemon';
 import POKEMONS from '../../models/mock-pokemon';
 import PokemonCard from '../../components/PokemonCard';
+import usePokemons from '../../hooks/usePokemons';
 
 const PokemonList: FunctionComponent = () => {
-	const [ pokemons, setPokemons ] = useState<Pokemon[]>([]);
-
-	useEffect(() => {
-		setPokemons(POKEMONS);
-	}, []);
+	const pokemons = usePokemons();
 
 	return (
 		<div>
